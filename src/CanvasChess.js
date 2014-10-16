8711bda058d6a58908a230f869876429701c7e7b
Andrew Ballantyne <andrew_ballantyne@rogers.com>
2014-10-15 21:32:41 -0400

    jQuery extraction

--- src/CanvasChess.js
+++ src/CanvasChess.js
@@ -35,11 +35,11 @@ var CanvasChess = (function (isAbstract) {
 
 	/* ----- Private Variables ----- */
 	_CanvasChess.prototype._canvasId = 'canvasChess';
-	/** @type jQuery **/
+	/** @type DOMElement **/
 	_CanvasChess.prototype._canvasTag = null;
 	/** @type createjs.Stage **/
 	_CanvasChess.prototype._stage = null;
-	/** @type jQuery **/
+	/** @type DOMElement **/
 	_CanvasChess.prototype._containerTag = null;
 	_CanvasChess.prototype._containerHasFixedHeight = false;
 	_CanvasChess.prototype._canvasBorderBuffer = 15;
@@ -54,16 +54,16 @@ var CanvasChess = (function (isAbstract) {
 	 * @private
 	 */
 	function _createCanvas(containerId) {
-		this._containerTag = $('#' + containerId);
+		this._containerTag = document.getElementById(containerId);
 		// If we have a fixed height (something in the style/css classes) we need to know that; if not, we have to do special calculations
-		this._containerHasFixedHeight = this._containerTag.height() != null && this._containerTag.height() > 0;
+		this._containerHasFixedHeight = this._containerTag.offsetHeight > 0;
 
-		this._canvasTag = $('<canvas></canvas>');
-		this._canvasTag.prop('id', this._canvasId);
+		this._canvasTag = document.createElement('canvas');
+		this._canvasTag.id = this._canvasId;
 
 		_calculateSize.call(this);
 
-		this._containerTag.append(this._canvasTag);
+		this._containerTag.appendChild(this._canvasTag);
 
 		var stage = new createjs.Stage(this._canvasId);
 		this._stage = stage;
@@ -73,33 +73,31 @@ var CanvasChess = (function (isAbstract) {
 		});
 	}
 	function _calculateSize() {
-		var width = this._containerTag.width();
-		var windowObj = $(window);
+		var width = this._containerTag.offsetWidth;
 		var height;
 		if (this._containerHasFixedHeight) {
 			// Container has a fixed height, let's use it
-			height = this._containerTag.height();
+			height = this._containerTag.offsetHeight;
 		} else {
 			// No Fixed height, a common DOM situation, as the container does not have a set height and will expand as content fills it.
 			// Since we need a height, let's do some quick calculations based on the window size and the location of the container from
 			// the top
-			height = windowObj.height() - (this._containerTag.offset().top * 2); // 2: 1 for the top, and 1 for a buffer at the bottom
+			height = window.innerHeight - (this._containerTag.getBoundingClientRect().top * 2); // *2: for the top offset + a bottom buffer
 		}
-		this._canvasTag.prop('width', width);
-		this._canvasTag.prop('height', height);
+		this._canvasTag.width = width;
+		this._canvasTag.height = height;
 	}
 
 	/**
-	 * @param options {Object?} - Optional. List of options to configure the base operation
+	 * @param options {Object} - List of options to configure the base operation
 	 * @private
 	 */
 	function _parseOptions(options) {
 		// Parse the options
 	}
-
 	function _setupResizeListener() {
 		var _this = this;
-		$(window).on('resize', function (e) {
+		window.addEventListener('resize', function (e) {
 			setTimeout(function() {
 				_calculateSize.call(_this, e);
 
@@ -107,7 +105,6 @@ var CanvasChess = (function (isAbstract) {
 			}, 0);
 		});
 	}
-
 	function _renderFunction() {
 		this._board = new ChessBoard(_getLength.call(this));
 		this._board.x = this._canvasBorderBuffer;
@@ -115,9 +112,8 @@ var CanvasChess = (function (isAbstract) {
 		this._updateRenderObjects.push(this._board);
 		this._stage.addChild(this._board);
 	}
-
 	function _getLength() {
-		return Math.min(this._canvasTag.height() - this._canvasBorderBuffer*2, this._canvasTag.width() - this._canvasBorderBuffer*2);
+		return Math.min(this._canvasTag.height - this._canvasBorderBuffer*2, this._canvasTag.width - this._canvasBorderBuffer*2);
 	}
 
 	/**
